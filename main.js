4020e69ccfae96c18f4c1416b4bb85cee0fee2c1
sktp06 <kantaporn_s@cmu.ac.th>
2022-07-16 14:08:05 +0700

    5.4

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
+            cart: [],
             premium: true
 
         }
     },
 
     methods: {
-        updateCart() {
-            this.cart +=1;
+        updateCart(id) {
+            this.cart.push(id);
         }
     }
 })
