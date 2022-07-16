c7d009ea3cfe340925c447b2af095f5873b5e981
sktp06 <kantaporn_s@cmu.ac.th>
2022-07-16 13:11:56 +0700

    4.4 and 4.5

--- main.js
+++ main.js
@@ -1,45 +1,45 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Shoes',
-            // 3.1
-            brand: 'SE 331',
-            // image: './assets/images/socks_green.jpg',
-            // inStock: false,
-            inventory: 100,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 }
-            ],
-            selectedVariant:0,
-            cart: 0,
+            // product: 'Shoes',
+            // // 3.1
+            // brand: 'SE 331',
+            // // image: './assets/images/socks_green.jpg',
+            // // inStock: false,
+            // inventory: 100,
+            // details: ['50% cotton', '30% wool', '20% polyester'],
+            // variants: [
+            //     { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+            //     { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 }
+            // ],
+            // selectedVariant:0,
+            cart: 0
             // 3.5
-            onSale: true
+            // onSale: true
 
-        }
-    },
-    computed: {
-        title() {
-            return this.brand + ' ' + this.product + (this.onSale ? " On Sale" : "")
-        },
-        image() {
-            return this.variants[this.selectedVariant].image
-        },
-        inStock() {
-            return this.variants[this.selectedVariant].quantity
-        }
-    },
-
-    methods: {
-        addToCart() {
-            this.cart += 1
-        },
-        updateImage(variantImage) {
-            this.image = variantImage
-        },
-        updateVariant(index) {
-            this.selectedVariant = index;
         }
     }
+    // computed: {
+    //     title() {
+    //         return this.brand + ' ' + this.product + (this.onSale ? " On Sale" : "")
+    //     },
+    //     image() {
+    //         return this.variants[this.selectedVariant].image
+    //     },
+    //     inStock() {
+    //         return this.variants[this.selectedVariant].quantity
+    //     }
+    // },
+
+    // methods: {
+    //     addToCart() {
+    //         this.cart += 1
+    //     },
+    //     updateImage(variantImage) {
+    //         this.image = variantImage
+    //     },
+    //     updateVariant(index) {
+    //         this.selectedVariant = index;
+    //     }
+    // }
 })
