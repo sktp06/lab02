f704fb50effd8bcddfaf596fe09cd61486500060
sktp06 <kantaporn_s@cmu.ac.th>
2022-07-16 12:08:58 +0700

    3.1

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Shoes',
+            // 3.1
+            brand: 'SE 331',
             image: './assets/images/socks_green.jpg',
             inStock: false,
             inventory: 100,
