d5b3e2cb222f9aa5b690255900f0ffe8b0fba93a
sktp06 <kantaporn_s@cmu.ac.th>
2022-07-16 13:24:39 +0700

    4.7

--- main.js
+++ main.js
@@ -1,24 +1,11 @@
 const app = Vue.createApp({
     data() {
         return {
-            // product: 'Shoes',
-            // // 3.1
-            // brand: 'SE 331',
-            // // image: './assets/images/socks_green.jpg',
-            // // inStock: false,
-            // inventory: 100,
-            // details: ['50% cotton', '30% wool', '20% polyester'],
-            // variants: [
-            //     { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-            //     { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 }
-            // ],
-            // selectedVariant:0,
-            cart: 0
-            // 3.5
-            // onSale: true
+            cart: 0,
+            premium: true
 
         }
-    }
+    },
     // computed: {
     //     title() {
     //         return this.brand + ' ' + this.product + (this.onSale ? " On Sale" : "")
