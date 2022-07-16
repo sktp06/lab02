1673e2a4b2dd7b7227fdddf36a84516972de3b5f
sktp06 <kantaporn_s@cmu.ac.th>
2022-07-16 14:01:24 +0700

    5.2

--- main.js
+++ main.js
@@ -6,27 +6,10 @@ const app = Vue.createApp({
 
         }
     },
-    // computed: {
-    //     title() {
-    //         return this.brand + ' ' + this.product + (this.onSale ? " On Sale" : "")
-    //     },
-    //     image() {
-    //         return this.variants[this.selectedVariant].image
-    //     },
-    //     inStock() {
-    //         return this.variants[this.selectedVariant].quantity
-    //     }
-    // },
 
-    // methods: {
-    //     addToCart() {
-    //         this.cart += 1
-    //     },
-    //     updateImage(variantImage) {
-    //         this.image = variantImage
-    //     },
-    //     updateVariant(index) {
-    //         this.selectedVariant = index;
-    //     }
-    // }
+    methods: {
+        updateCart() {
+            this.cart +=1;
+        }
+    }
 })
