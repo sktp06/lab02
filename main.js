5a8e6b456417fac23757a2ac441f067f7d22451b
sktp06 <kantaporn_s@cmu.ac.th>
2022-07-16 12:23:54 +0700

    3.4

--- main.js
+++ main.js
@@ -4,14 +4,15 @@ const app = Vue.createApp({
             product: 'Shoes',
             // 3.1
             brand: 'SE 331',
-            image: './assets/images/socks_green.jpg',
-            inStock: false,
+            // image: './assets/images/socks_green.jpg',
+            // inStock: false,
             inventory: 100,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 }
             ],
+            selectedVariant:0,
             cart: 0
 
         }
@@ -19,15 +20,24 @@ const app = Vue.createApp({
     computed: {
         title() {
             return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
         }
     },
-    
+
     methods: {
         addToCart() {
             this.cart += 1
         },
         updateImage(variantImage) {
             this.image = variantImage
+        },
+        updateVariant(index) {
+            this.selectedVariant = index;
         }
     }
 })
